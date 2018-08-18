542fdc8a3107f3d6258f5772769a2fa591f60e19
JoritPc <Jorit.vergalle@hotmail.com>
2018-08-18 21:47:10 +0200

    Leakcanary implemented

--- app/src/main/java/com/example/jorit/images_app/App.java
+++ app/src/main/java/com/example/jorit/images_app/App.java
@@ -1,8 +1,11 @@
 package com.example.jorit.images_app;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.example.jorit.images_app.domain.MyObjectBox;
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
 
 import io.objectbox.BoxStore;
 
@@ -14,6 +17,13 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
 
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+        //LeakCanary.enableDisplayLeakActivity(this);
         // do this once, for example in your Application class
         boxStore = MyObjectBox.builder().androidContext(App.this).build();
 // do this in your activities/fragments to get hold of a Box
